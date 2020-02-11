bfa8226d6b3378f68405039203d2edb7cc3d1ec0
shruti.pahuja <shruti.pahuja@accenture.com>
2020-02-11 14:14:43 +0530

    Added POJO class for database

--- app/src/main/java/com/example/notes/CreateNoteActivity.java
+++ app/src/main/java/com/example/notes/CreateNoteActivity.java
@@ -3,15 +3,21 @@ package com.example.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 /**
  * Screen 2 - Create Note Screen allowing to create a new note
  */
 public class CreateNoteActivity extends AppCompatActivity {
 
+    EditText noteTitleEditText, noteContentEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        noteTitleEditText = findViewById(R.id.noteTitleEditText);
+        noteContentEditText = findViewById(R.id.noteContentEditText);
     }
 }
---
+++ app/src/main/java/com/example/notes/Note.java
@@ -0,0 +1,74 @@
+package com.example.notes;
+
+/**
+ * POJO Class for inserting and retreiving of data variables from/to database
+ */
+
+public class Note {
+
+    private long id;
+    private String title;
+    private String content;
+    private String date;
+    private String time;
+
+    public Note() {
+
+    }
+
+    public Note(String title, String content, String date, String time) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Note(long id, String title, String content, String date, String time) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.time = time;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
