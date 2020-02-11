c7962c108bcc871c2eb9b18e0f77c7ef524c1263
shruti.pahuja <shruti.pahuja@accenture.com>
2020-02-11 12:35:40 +0530

    Added Create Note Screen Added FAB for accessing Create Note Screen Added dependency - library for FAB

---
+++ app/src/main/java/com/example/notes/CreateNoteActivity.java
@@ -0,0 +1,17 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Screen 2 - Create Note Screen allowing to create a new note
+ */
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+    }
+}
--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+/**
+ * Screen 1 - List Notes Screen displaying list of notes
+ */
+
 public class MainActivity extends AppCompatActivity {
+    Toolbar notesListToolbar;
+    RecyclerView notesListRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesListToolbar = findViewById(R.id.notesListToolbar);
+        setSupportActionBar(notesListToolbar);
+
+        notesListRecyclerView = findViewById(R.id.notesListRecyclerView);
+
+
     }
 }
